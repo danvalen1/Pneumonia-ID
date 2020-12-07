854a645ba64655a8be8da74006505ef8ac1f80d1
Daniel Valenzuela <danvalen1@gmail.com>
2020-12-07 13:18:29 -0800

    updating data EDA/prep

--- src/modules/VizAx.py
+++ src/modules/VizAx.py
@@ -2,9 +2,11 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib
+from matplotlib.colors import ListedColormap
 
-fontscale = 2
+fontscale = 1.5
 sns.set(font_scale=fontscale, style='whitegrid')
+figsize = (12,8)
 
 def channelScatter(index_df):
     grey_img = index_df[index_df.channels == 1]
@@ -24,3 +26,24 @@ def channelScatter(index_df):
     plt.savefig("images/image_colors.png")
     return plt.show()
     
+def subsetsJoint(index_df):
+    jointplot = sns.jointplot(data=index_df, 
+                           x="width", 
+                           y="height", 
+                           hue="data_set_name", 
+                           kind="scatter",
+                           height = figsize[1],
+                           ratio = round(figsize[1]/figsize[0]+5),
+                           alpha=0.4)
+
+
+    jointplot.ax_joint.legend(title="Subset")
+    jointplot.ax_joint.set_xlabel("Width of Image")
+    jointplot.ax_joint.set_ylabel("Height of Image")
+    jointplot.ax_joint.set_ylabel("Height of Image")
+    plt.suptitle("Distribution of Image Dimensions\nby Data Subset Membership", size=18)
+    
+    plt.tight_layout()
+    plt.savefig("images/image_pix_dist.png")
+    
+    return plt.show()
